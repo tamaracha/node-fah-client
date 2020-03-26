f368ed7cc4a0fc17c8d77afd7dc3a15435bda8d2
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-03-26 17:09:19 +0100

    Add npm script to format js code with prettier-standard

--- types/commands/index.d.ts
+++ types/commands/index.d.ts
@@ -1,5 +1,5 @@
-export * as control from "./control.js";
 export * as helpers from "./helpers.js";
+export * as control from "./control.js";
 export * as options from "./options.js";
 export * as slots from "./slots.js";
 export * as updates from "./updates.js";
