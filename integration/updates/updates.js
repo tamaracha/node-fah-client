2572549146287cf72b90d1bde28d9faca8bc4048
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-04-18 22:48:55 +0200

    Extend updates integration test

--- integration/updates/updates.js
+++ integration/updates/updates.js
@@ -3,6 +3,7 @@ import { FahClient, control, options, slots, updates, work } from '../../lib/ind
 const fc = new FahClient()
 
 async function demo () {
+  fc.onMessage(m => console.log(m))
   const updateCommands = [
     { id: 0, rate: 5, payload: control.heartbeat() },
     { id: 1, rate: 5, payload: slots.list() },
@@ -10,9 +11,12 @@ async function demo () {
     { id: 3, rate: 5, payload: options.listByFilter('all') }
   ]
   await fc.connect()
+  const h = await fc.dispatch(updateCommands[0].payload)
+  console.log(h)
   await fc.dispatch(updates.clear())
   for (const c of updateCommands) {
-    await fc.dispatch(updates.create(c))
+    const created = await fc.dispatch(updates.create(c))
+    console.log(created)
   }
   const list = await fc.dispatch(updates.list())
   console.log(list)
