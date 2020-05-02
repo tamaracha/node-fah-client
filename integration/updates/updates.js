5efaf74973907c952465cf415e867684c7f77a28
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-05-02 12:07:02 +0200

    Use a transform stream for tokenizing in FahClient

--- integration/updates/updates.js
+++ integration/updates/updates.js
@@ -1,25 +1,22 @@
 'use strict'
-import { FahClient, control, options, slots, updates, work } from '../../lib/index.js'
-const fc = new FahClient()
+const { FahClient, control, options, slots, updates, work } = require('../../dist/index.js')
 
 async function demo () {
-  fc.onMessage(m => console.log(m))
+  const fc = await FahClient.connect()
+  fc.events.on('token', token => console.log(token.type))
   const updateCommands = [
-    { id: 0, rate: 5, payload: control.heartbeat() },
+    // { id: 0, rate: 5, payload: control.heartbeat() },
     { id: 1, rate: 5, payload: slots.list() },
     { id: 2, rate: 5, payload: work.units() },
     { id: 3, rate: 5, payload: options.listByFilter('all') }
   ]
-  await fc.connect()
-  const h = await fc.dispatch(updateCommands[0].payload)
-  console.log(h)
   await fc.dispatch(updates.clear())
   for (const c of updateCommands) {
-    const created = await fc.dispatch(updates.create(c))
-    console.log(created)
+    fc.dispatch(updates.create(c))
+    .then(created => console.log(created))
   }
   const list = await fc.dispatch(updates.list())
   console.log(list)
-  await fc.disconnect()
+  // await fc.disconnect()
 }
 demo()
