6cbebe28f72ee49b47b2b766888017373cdf4ac4
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-12-29 21:02:15 +0100

    Update build deps for linting, docs, and tests
    - Coverage: Replace nyc and tap-nyc with c8
    - lint-staged: check complete typescript project

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,11 +1,7 @@
 'use strict'
 module.exports = {
   '*.{json,yml,yaml}': 'prettier --write',
-  '(lib|config)/**/*.?(c|m)js': files => `standard --fix ${files.join(' ')} | snazzy`,
-  'lib/**/!(*.spec).js': [
-    'tsc --noEmit --allowJs --checkJs --allowSyntheticDefaultImports --moduleResolution node --target es2019 --lib es2019 --module esnext --strict'
-  ],
-  'lib/**/*.spec.js': [
-    'tsc --noEmit --allowJs --checkJs --allowSyntheticDefaultImports --moduleResolution node --target es2019 --lib es2019 --module esnext '
-  ]
+  '(integration|lib)/**/*.?(c|m)js': 'standard --fix',
+  'lib/**/!(*.spec).js': _ => 'tsc --noEmit -p config/tsconfig.node12.json',
+  'lib/**/*.spec.js': _ => 'tsc --noEmit -p config/tsconfig.node12.json'
 }
