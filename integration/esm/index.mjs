dcc36ce443235bc9696ebecfcdf236145ae3fe85
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2020-12-30 17:04:37 +0100

    Fix style issues in integration tests

--- integration/esm/index.mjs
+++ integration/esm/index.mjs
@@ -6,13 +6,13 @@ const cmd = control.heartbeat()
 
 async function demo () {
   try {
-  // connect to the fah interface
-  const fah = await FahClient.connect()
+    // connect to the fah interface
+    const fah = await FahClient.connect()
     fah.events.once('ready', init => console.log('init: ', init))
-  const heartbeat = await fah.dispatch(cmd)
-  console.log(heartbeat)
-  // After work is done, disconnect from fah interface
-  await fah.end()
+    const heartbeat = await fah.dispatch(cmd)
+    console.log(heartbeat)
+    // After work is done, disconnect from fah interface
+    await fah.end()
     console.log('Goodbye')
   } catch (e) {
     console.log(e)
--- integration/updates/updates.cjs
+++ integration/updates/updates.cjs
@@ -5,7 +5,7 @@ async function demo () {
   const fc = await FahClient.connect()
   fc.events.on('token', token => console.log(token.type))
   const updateCommands = [
-    // { id: 0, rate: 5, payload: control.heartbeat() },
+    { id: 0, rate: 5, payload: control.heartbeat() },
     { id: 1, rate: 5, payload: slots.list() },
     { id: 2, rate: 5, payload: work.units() },
     { id: 3, rate: 5, payload: options.listByFilter('all') }
@@ -13,7 +13,6 @@ async function demo () {
   await fc.dispatch(updates.clear())
   for (const c of updateCommands) {
     fc.dispatch(updates.create(c))
-    .then(created => console.log(created))
   }
   const list = await fc.dispatch(updates.list())
   console.log(list)
