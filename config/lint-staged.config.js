399cfc59eb945e1e7f882ba4b1831cfdeeb8f8bb
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2021-01-03 00:51:56 +0100

    Lint-staged: Include config directory in js linting

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   '*.{json,yml,yaml}': 'prettier --write',
-  '(integration|lib)/**/*.?(c|m)js': 'standard --fix',
+  '(config|integration|lib)/**/*.?(c|m)js': 'standard --fix',
   'lib/**/!(*.spec).js': _ => 'tsc --noEmit -p config/tsconfig.node12.json',
   'lib/**/*.spec.js': _ => 'tsc --noEmit -p config/tsconfig.node12.json'
 }
